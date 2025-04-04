cb464fddc9cb2b9b4a78bdcae944780673141497
Rohan <rohanjain0725@gmail.com>
2025-04-04 09:20:25 -0500

    Fix id's generating sequentially

--- inital.sql
+++ inital.sql
@@ -1,11 +1,13 @@
+/* This is used when the database is first initalized and prepped for prod */
+
 CREATE TABLE IF NOT EXISTS playerData (
     username VARCHAR(255),
     balance float DEFAULT 1000,
-    id SERIAL PRIMARY KEY,
+    id PRIMARY KEY,
     plinkoPlayed integer DEFAULT 0,
     coinFlipPlayed integer DEFAULT 0,
     wheelOfFortunePlayed integer DEFAULT 0
 );
 
-INSERT INTO playerData (username) 
-VALUES ('exampleUser');
+INSERT INTO playerData (username, id) 
+VALUES ('exampleUser', 1234);
